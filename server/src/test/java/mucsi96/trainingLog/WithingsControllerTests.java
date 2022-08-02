ddafd05f3863c09c1112a2e6068d4afce5c5a8a7
victory523 <richardstuward523@gmail.com>
2022-08-02 21:00:26 +0200

    Simplify test

--- server/src/test/java/mucsi96/trainingLog/WithingsControllerTests.java
+++ server/src/test/java/mucsi96/trainingLog/WithingsControllerTests.java
@@ -1,33 +1,25 @@
 package mucsi96.trainingLog;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class WithingsControllerTests {
 
     @Autowired
-    private WebApplicationContext context;
-
-    private MockMvc mvc;
-
-    @BeforeEach
-    public void setup() {
-        this.mvc = MockMvcBuilders.webAppContextSetup(context).build();
-    }
+    private MockMvc mockMvc;
 
     @Test
     public void returnsUnauthorizedWithoutBearerToken() throws Exception {
-        this.mvc.perform(get("/withings/weight"))
+        mockMvc.perform(get("/withings/weight"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$._links.oauth2Login.href")
                         .value("/api/oauth2/authorization/withings-client")
