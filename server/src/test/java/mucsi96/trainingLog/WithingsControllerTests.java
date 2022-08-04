8c3813be7d9ad52980d2d927f3c3155579f46afe
victory523 <richardstuward523@gmail.com>
2022-08-04 12:07:58 +0200

    use MockMvc

--- server/src/test/java/mucsi96/trainingLog/WithingsControllerTests.java
+++ server/src/test/java/mucsi96/trainingLog/WithingsControllerTests.java
@@ -1,11 +1,16 @@
 package mucsi96.trainingLog;
 
+import org.assertj.core.matcher.AssertionMatcher;
+import org.hamcrest.DiagnosingMatcher;
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -14,58 +19,63 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WithingsControllerTests {
 
     @Autowired
-    private WebTestClient webTestClient;
+    private MockMvc mvc;
 
     @Test
-    public void returns_unauthorized_if_bearer_token_is_not_sent() {
-        webTestClient
-                .get()
-                .uri("/withings/weight")
-                .exchange()
-                .expectStatus().isUnauthorized()
-                .expectBody()
-                    .jsonPath("$._links.oauth2Login.href")
-                    .isEqualTo("/api/oauth2/authorization/withings-client");
+    public void returns_unauthorized_if_bearer_token_is_not_sent() throws Exception {
+        mvc
+                .perform(get("/withings/weight"))
+                .andExpect(status().isUnauthorized())
+                .andExpect(
+                        jsonPath("$._links.oauth2Login.href")
+                                .value("/api/oauth2/authorization/withings-client")
+                );
     }
 
     @Test
-    public void redirects_to_withings_oauth_page_on_calling_oauth2Login() {
-        webTestClient
-                .get()
-                .uri("/oauth2/authorization/withings-client")
-                .exchange()
-                .expectStatus().isFound()
-                .expectHeader().value(HttpHeaders.LOCATION, location -> {
-                    UriComponents components = UriComponentsBuilder.fromUriString(location).build();
-                    assertEquals("localhost", components.getHost());
-                    assertEquals(8080, components.getPort());
-                    assertEquals("/oauth2_user/authorize2", components.getPath());
-                    assertEquals(
-                            "code",
-                            components.getQueryParams().get(OAuth2ParameterNames.RESPONSE_TYPE).get(0)
-                    );
-                    assertEquals(
-                            "test-withings-client-id",
-                            components.getQueryParams().get(OAuth2ParameterNames.CLIENT_ID).get(0)
-                    );
-                    assertEquals(
-                            "user.metrics",
-                            components.getQueryParams().get(OAuth2ParameterNames.SCOPE).get(0)
-                    );
-                    assertEquals(
-                            Base64.getUrlDecoder().decode(
-                                    URLDecoder.decode(
-                                            components.getQueryParams().get(OAuth2ParameterNames.STATE).get(0),
-                                            StandardCharsets.UTF_8
-                                    )
-                            ).length,
-                            32
-                    );
-                });
+    public void redirects_to_withings_oauth_page_on_calling_oauth2Login() throws Exception {
+        mvc
+                .perform(get("/oauth2/authorization/withings-client"))
+                        .andExpect(status().isFound())
+                        .andExpect(header().string(HttpHeaders.LOCATION, new AssertionMatcher<String>() {
+                            @Override
+                            public void assertion(String location) throws AssertionError {
+                                UriComponents components = UriComponentsBuilder.fromUriString(location).build();
+                                assertEquals("localhost", components.getHost());
+                                assertEquals(8080, components.getPort());
+                                assertEquals("/oauth2_user/authorize2", components.getPath());
+                                assertEquals(
+                                        "code",
+                                        components.getQueryParams().get(OAuth2ParameterNames.RESPONSE_TYPE).get(0)
+                                );
+                                assertEquals(
+                                        "test-withings-client-id",
+                                        components.getQueryParams().get(OAuth2ParameterNames.CLIENT_ID).get(0)
+                                );
+                                assertEquals(
+                                        "user.metrics",
+                                        components.getQueryParams().get(OAuth2ParameterNames.SCOPE).get(0)
+                                );
+                                assertEquals(
+                                        Base64.getUrlDecoder().decode(
+                                                URLDecoder.decode(
+                                                        components.getQueryParams().get(OAuth2ParameterNames.STATE).get(0),
+                                                        StandardCharsets.UTF_8
+                                                )
+                                        ).length,
+                                        32
+                                );
+                            }
+                        }));
+
     }
 }
