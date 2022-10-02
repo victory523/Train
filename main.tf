1ed7f96543eea4b147d02fef190559192839e1d2
victory523 <richardstuward523@gmail.com>
2022-10-02 14:16:22 +0200

    use id

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "kubernetes_namespace" "workout" {
 
 module "client" {
   source     = "./client/deploy"
-  namespace  = kubernetes_namespace.workout.metadata.name
+  namespace  = kubernetes_namespace.workout.id
   image_name = "mucsi96/${kubernetes_namespace.workout.metadata.name}-client"
 }
