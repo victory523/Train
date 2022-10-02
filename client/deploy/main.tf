54b712da5961b8740cc8927cbe9fd930794b5737
victory523 <richardstuward523@gmail.com>
2022-10-02 14:37:06 +0200

    fix path

--- client/deploy/main.tf
+++ client/deploy/main.tf
@@ -1,7 +1,7 @@
 module "image_version" {
   source     = "github.com/mucsi96/terraform-modules//version"
   tag_prefix = "client-image"
-  path       = ".."
+  path       = "${path.module}/.."
   ignore     = ["deploy/"]
 }
 
@@ -9,13 +9,13 @@ module "chart_version" {
   source      = "github.com/mucsi96/terraform-modules//helm-chart-version"
   app_version = module.image_version.version
   tag_prefix  = "client-chart"
-  path        = "."
+  path        = path.module
 }
 
 resource "null_resource" "image" {
   provisioner "local-exec" {
     command = <<-EOT
-      docker build .. \
+      docker build ${path.module}/.. \
         --quiet \
         --tag ${var.image_name}:${module.image_version.version} \
         --tag ${var.image_name}:latest
@@ -33,7 +33,7 @@ resource "helm_release" "chart" {
   name      = var.host
   version   = module.chart_version.version
   namespace = var.namespace
-  chart     = "."
+  chart     = path.module
   depends_on = [
     resource.null_resource.image
   ]
