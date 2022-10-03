aee53c6a818bfd5adf907576d113d1a344b12bc6
victory523 <richardstuward523@gmail.com>
2022-10-03 08:15:19 +0200

    Add port foward

---
+++ database/port-forward.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+username=$(kubectl get secret app-database -n workout -o jsonpath="{.data.userName}" | base64 --decode)
+echo "Username: $username"
+kubectl port-forward services/app-database 3308:http -n workout
