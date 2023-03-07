6fd0b9827b06d28039eb67e1ea091c912fe7a4ba
victory523 <richardstuward523@gmail.com>
2023-03-07 20:34:19 +0000

    deploy db

--- database/deploy/exporter-grant.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER '${exporter_username}' IDENTIFIED BY '${exporter_password}' WITH MAX_USER_CONNECTIONS 3;
-
-GRANT USAGE, PROCESS, REPLICATION CLIENT, SELECT ON *.* TO '${exporter_username}';
--- database/port-forward.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-username=$(kubectl get secret app-database -n workout -o jsonpath="{.data.username}" | base64 --decode)
-echo "Username: $username"
-kubectl port-forward services/app-database 3308:http -n workout
