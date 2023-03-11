9a794c9f3fc8487baea900107249eada96bef0be
victory523 <richardstuward523@gmail.com>
2023-03-11 21:05:31 +0000

    Revert annotation removal

--- server/src/test/java/mucsi96/trainingLog/ServerApplicationTests.java
+++ server/src/test/java/mucsi96/trainingLog/ServerApplicationTests.java
@@ -2,7 +2,9 @@ package mucsi96.trainingLog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class ServerApplicationTests {
 
