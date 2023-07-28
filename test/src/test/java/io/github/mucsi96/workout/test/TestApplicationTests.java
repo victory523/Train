cda193983e22a41c5a220f743baad20598aa0a81
victory523 <richardstuward523@gmail.com>
2023-07-28 07:13:01 +0000

    add test application

---
+++ test/src/test/java/io/github/mucsi96/workout/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mucsi96.workout.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
