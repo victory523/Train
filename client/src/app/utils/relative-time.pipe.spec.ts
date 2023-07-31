4d7fb32a68e7f0ef3a39004d4fbf4e72dd96993a
victory523 <richardstuward523@gmail.com>
2023-07-31 16:28:18 +0000

    fix RelativeTimePipe tests

--- client/src/app/utils/relative-time.pipe.spec.ts
+++ client/src/app/utils/relative-time.pipe.spec.ts
@@ -2,78 +2,80 @@ import { RelativeTimePipe } from './relative-time.pipe';
 
 describe('RelativeTimePipe', () => {
   [
-    { date: new Date(), result: ['now', '1 second ago'] },
+    { getDate: () => new Date(), result: 'now' },
     {
-      date: new Date(Date.now() - 999),
-      result: ['1 second ago', '2 seconds ago'],
+      getDate: () => new Date(Date.now() - 999),
+      result: '1 second ago',
     },
     {
-      date: new Date(Date.now() - 1000),
-      result: ['1 second ago', '2 seconds ago'],
+      getDate: () => new Date(Date.now() - 1000),
+      result: '1 second ago',
     },
     {
-      date: new Date(Date.now() - 59 * 1000),
-      result: ['59 seconds ago', '1 minute ago'],
+      getDate: () => new Date(Date.now() - 59 * 1000),
+      result: '59 seconds ago',
     },
-    { date: new Date(Date.now() - 60 * 1000), result: '1 minute ago' },
-    { date: new Date(Date.now() - 59 * 60 * 1000), result: '59 minutes ago' },
-    { date: new Date(Date.now() - 60 * 60 * 1000), result: '1 hour ago' },
+    { getDate: () => new Date(Date.now() - 60 * 1000), result: '1 minute ago' },
     {
-      date: new Date(Date.now() - 23 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 59 * 60 * 1000),
+      result: '59 minutes ago',
+    },
+    {
+      getDate: () => new Date(Date.now() - 60 * 60 * 1000),
+      result: '1 hour ago',
+    },
+    {
+      getDate: () => new Date(Date.now() - 23 * 60 * 60 * 1000),
       result: '23 hours ago',
     },
     {
-      date: new Date(Date.now() - 1 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 1 * 24 * 60 * 60 * 1000),
       result: 'yesterday',
     },
     {
-      date: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 2 * 24 * 60 * 60 * 1000),
       result: '2 days ago',
     },
     {
-      date: new Date(Date.now() - 6 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 6 * 24 * 60 * 60 * 1000),
       result: '6 days ago',
     },
     {
-      date: new Date(Date.now() - 1 * 7 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 1 * 7 * 24 * 60 * 60 * 1000),
       result: 'last week',
     },
     {
-      date: new Date(Date.now() - 4 * 7 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 4 * 7 * 24 * 60 * 60 * 1000),
       result: '4 weeks ago',
     },
     {
-      date: new Date(Date.now() - 29 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 29 * 24 * 60 * 60 * 1000),
       result: '4 weeks ago',
     },
     {
-      date: new Date(Date.now() - 1 * 30 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 1 * 30 * 24 * 60 * 60 * 1000),
       result: 'last month',
     },
     {
-      date: new Date(Date.now() - 12 * 30 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 12 * 30 * 24 * 60 * 60 * 1000),
       result: '12 months ago',
     },
     {
-      date: new Date(Date.now() - 364 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 364 * 24 * 60 * 60 * 1000),
       result: '12 months ago',
     },
     {
-      date: new Date(Date.now() - 1 * 365 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 1 * 365 * 24 * 60 * 60 * 1000),
       result: 'last year',
     },
     {
-      date: new Date(Date.now() - 5 * 365 * 24 * 60 * 60 * 1000),
+      getDate: () => new Date(Date.now() - 5 * 365 * 24 * 60 * 60 * 1000),
       result: '5 years ago',
     },
-  ].forEach(({ date, result }) =>
-    it(`formats ${date} as ${result}`, () => {
+  ].forEach(({ getDate, result }) =>
+    it(`formats ${result}`, () => {
       const pipe = new RelativeTimePipe();
-      if (Array.isArray(result)) {
-        expect(result).toContain(pipe.transform(date));
-      } else {
-        expect(pipe.transform(date)).toBe(result);
-      }
+      expect(pipe.transform(getDate())).toBe(result);
     })
   );
 });
