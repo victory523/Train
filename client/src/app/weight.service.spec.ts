246706082ac4d18310b1fe647bb3e464b5fe1b29
victory523 <richardstuward523@gmail.com>
2023-08-13 18:55:58 +0000

    try to fix client tests

--- client/src/app/weight.service.spec.ts
+++ client/src/app/weight.service.spec.ts
@@ -1,24 +1,32 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { WeightResponse, WeightService } from './weight.service';
+import { WeightMeasurement, WeightService } from './weight.service';
 
 function setup() {
   TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule]
+    imports: [HttpClientTestingModule],
   });
   const service = TestBed.inject(WeightService);
-  const httpTestingController = TestBed.inject(HttpTestingController)
-  return {service, httpTestingController}
+  const httpTestingController = TestBed.inject(HttpTestingController);
+  return { service, httpTestingController };
 }
 
 describe('WeightService', () => {
   describe('getWeight', () => {
-    it('should return weight',  () => {
+    it('should return weight', () => {
       const { service, httpTestingController } = setup();
-      service.getWeight().subscribe(weight => {
-        expect(weight).toEqual(89.6);
-      })
-      httpTestingController.expectOne('/api/weight').flush({ weight: 89.6 } as WeightResponse)
+      const mockResponse: WeightMeasurement[] = [
+        { date: new Date('2020-05-05T00:00:00.000Z'), weight: 108.9 },
+        { date: new Date('2020-05-07T00:00:00.000Z'), weight: 108.3 },
+        { date: new Date('2020-05-10T00:00:00.000Z'), weight: 107.8 },
+      ];
+      service.getWeight(7).subscribe((measurements) => {
+        expect(measurements).toEqual(mockResponse);
+      });
+      httpTestingController.expectOne('/api/weight').flush(mockResponse);
       httpTestingController.verify();
     });
   });
--- client/src/app/weight/weight.component.spec.ts
+++ client/src/app/weight/weight.component.spec.ts
@@ -3,15 +3,17 @@ import { TestBed } from '@angular/core/testing';
 import { Subject } from 'rxjs';
 import { CommonComponentsModule } from '../common-components/common-components.module';
 import { NotificationService } from '../common-components/notification.service';
-import { WeightService } from '../weight.service';
+import { WeightMeasurement, WeightService } from '../weight.service';
 import { WeightComponent } from './weight.component';
 
 async function setup() {
-  const weightSubject = new Subject<number | undefined>();
+  const weightMeasurementSubject = new Subject<WeightMeasurement[]>();
   const mockWeightService: jasmine.SpyObj<WeightService> = jasmine.createSpyObj(
     ['getWeight']
   );
-  mockWeightService.getWeight.and.returnValue(weightSubject.asObservable());
+  mockWeightService.getWeight.and.returnValue(
+    weightMeasurementSubject.asObservable()
+  );
   const mockNotificationService: jasmine.SpyObj<NotificationService> =
     jasmine.createSpyObj(['showNotification']);
   await TestBed.configureTestingModule({
@@ -30,7 +32,7 @@ async function setup() {
     fixture,
     element: fixture.nativeElement as HTMLElement,
     mockNotificationService,
-    weightSubject,
+    weightSubject: weightMeasurementSubject,
   };
 }
 
@@ -42,14 +44,19 @@ describe('WeightComponent', () => {
 
   it('renders weight', async () => {
     const { element, fixture, weightSubject } = await setup();
-    weightSubject.next(87.6);
+    weightSubject.next([
+      {
+        date: new Date('2020-05-05T00:00:00.000Z'),
+        weight: 108.9,
+      },
+    ]);
     fixture.detectChanges();
     expect(element.querySelector('app-badge')?.textContent).toEqual('87.6');
   });
 
   it('renders question mark if no weight is returned', async () => {
     const { element, fixture, weightSubject } = await setup();
-    weightSubject.next(undefined);
+    weightSubject.next([]);
     fixture.detectChanges();
     expect(element.querySelector('app-badge')?.textContent).toEqual('-');
   });
