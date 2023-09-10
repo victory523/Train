e8546f31b6bf141cd9822d1ff4eed0edcab06b99
victory523 <richardstuward523@gmail.com>
2023-09-10 11:20:24 +0000

    fix all client tests

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -4,6 +4,7 @@ import { Component, Input } from '@angular/core';
 import { WeightComponent } from '../weight/weight.component';
 import { HomeComponent } from './home.component';
 import { By } from '@angular/platform-browser';
+import { RideComponent } from '../ride/ride.component';
 
 @Component({
   standalone: true,
@@ -14,16 +15,25 @@ class MockWeightComponent {
   period: number | undefined;
 }
 
+@Component({
+  standalone: true,
+  selector: 'app-ride',
+})
+class MockRideComponent {
+  @Input()
+  period: number | undefined;
+}
+
 async function setup() {
   TestBed.configureTestingModule({
     imports: [HomeComponent],
   });
   TestBed.overrideComponent(HomeComponent, {
     remove: {
-      imports: [WeightComponent],
+      imports: [WeightComponent, RideComponent],
     },
     add: {
-      imports: [MockWeightComponent],
+      imports: [MockWeightComponent, MockRideComponent],
     },
   });
   const fixture = TestBed.createComponent(HomeComponent);
@@ -41,6 +51,16 @@ describe('HomeComponent', () => {
     fixture.detectChanges();
     expect(
       fixture.debugElement.query(By.directive(MockWeightComponent))
+        .componentInstance.period
+    ).toBe(7);
+  });
+
+  it('renders ride with passed period', async () => {
+    const { fixture } = await setup();
+    fixture.componentInstance.period = 7;
+    fixture.detectChanges();
+    expect(
+      fixture.debugElement.query(By.directive(MockRideComponent))
         .componentInstance.period
     ).toBe(7);
   });
