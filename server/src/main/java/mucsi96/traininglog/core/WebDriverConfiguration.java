a828c293390f10d61435cb944993e2728a0b734b
victory523 <richardstuward523@gmail.com>
2023-09-14 11:40:13 +0000

    chore: fix webdriver configuration

--- server/src/main/java/mucsi96/traininglog/core/WebDriverConfiguration.java
+++ server/src/main/java/mucsi96/traininglog/core/WebDriverConfiguration.java
@@ -1,13 +1,18 @@
 package mucsi96.traininglog.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Level;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.Data;
 
@@ -18,22 +23,24 @@ public class WebDriverConfiguration {
   private String apiUri;
 
   @Bean
-  public WebDriver getWebDriver() {
-  ChromeOptions options = new ChromeOptions().addArguments("--headless",
-  "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage",
-  "--window-size=1920,1080", "--remote-allow-origins=*");
+  @Profile({ "test", "local" })
+  public WebDriver getLocalWebDriver() {
+    ChromeOptions options = new ChromeOptions().addArguments("--headless",
+        "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage",
+        "--window-size=1920,1080", "--remote-allow-origins=*");
 
-  ChromeDriver driver = new ChromeDriver(options);
-  driver.setLogLevel(Level.WARNING);
-  return driver;
+    ChromeDriver driver = new ChromeDriver(options);
+    driver.setLogLevel(Level.WARNING);
+    return driver;
   }
 
-  // @Bean
-  // public WebDriver getWebDriver() throws MalformedURLException {
-  //   ChromeOptions options = new ChromeOptions().addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+  @Bean
+  @Profile({ "prod" })
+  public WebDriver getRemoteWebDriver() throws MalformedURLException {
+    ChromeOptions options = new ChromeOptions().addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
 
-  //   RemoteWebDriver driver = new RemoteWebDriver(new URL(apiUri), options);
-  //   driver.setLogLevel(Level.WARNING);
-  //   return new Augmenter().augment(driver);
-  // }
+    RemoteWebDriver driver = new RemoteWebDriver(new URL(apiUri), options);
+    driver.setLogLevel(Level.WARNING);
+    return new Augmenter().augment(driver);
+  }
 }
