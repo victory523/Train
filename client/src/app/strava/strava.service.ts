57b0d59fdd1334b38c2a9b9b3066212622e926c3
victory523 <richardstuward523@gmail.com>
2023-11-09 12:15:49 +0000

    sync strava after withings in sequence

--- client/src/app/strava/strava.service.ts
+++ client/src/app/strava/strava.service.ts
@@ -1,26 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { EMPTY, catchError, shareReplay } from 'rxjs';
+import { EMPTY, catchError, mergeMap, of, shareReplay } from 'rxjs';
 import { NotificationService } from '../common-components/notification.service';
+import { WithingsService } from '../withings/withings.service';
 
 @Injectable()
 export class StravaService {
   constructor(
     private readonly http: HttpClient,
-    private readonly notificationService: NotificationService
+    private readonly notificationService: NotificationService,
+    private readonly withingsService: WithingsService
   ) {}
 
-  private readonly $syncActivities = this.http
-    .post<void>('/api/strava/activities/sync', undefined)
+  private readonly $syncActivities = this.withingsService
+    .syncMeasurements()
     .pipe(
-      catchError(() => {
-        this.notificationService.showNotification(
-          'Unable to sync with Strava',
-          'error'
-        );
-        return EMPTY;
-      }),
-      shareReplay(1)
+      mergeMap(() =>
+        this.http.post<void>('/api/strava/activities/sync', undefined).pipe(
+          catchError(() => {
+            this.notificationService.showNotification(
+              'Unable to sync with Strava',
+              'error'
+            );
+            return EMPTY;
+          }),
+          shareReplay(1)
+        )
+      )
     );
 
   syncActivities() {
